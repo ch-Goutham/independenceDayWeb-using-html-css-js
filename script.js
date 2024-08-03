09282ec81de2caa15c4ebec9dbd99570f6855915
ch-Goutham <gouthamyadav202@gmail.com>
2024-08-03 22:09:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.addEventListener("DOMContentLoaded", ()=> {
+    // Custom JS for form validation or other interactivity
+    document.querySelector('form').addEventListener('submit',()=>{
+        alert('Thank you for registering!');
+
+    })
+});
